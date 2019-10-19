9cce7767930abe01b96c3271789636fc4cc56cfc
Robert Vari <mail.robertvari@gmail.com>
2019-10-19 11:57:10 +0200

    New urls.py in Pages app

---
+++ Pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import home, contact, about
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('contact/', contact, name="contact"),
+    path('about/', about, name="about"),
+]
