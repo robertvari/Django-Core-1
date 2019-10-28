230dc6387a86adf99e84fda8983412e4e7ad4f2f
Robert Vari <mail.robertvari@gmail.com>
2019-10-28 22:56:20 +0100

    Test Category and Photo model

--- Photos/models.py
+++ Photos/models.py
@@ -8,3 +8,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='photos')
+    description = models.TextField()
+    uploaded = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Photos"
+    
+    def __str__(self):
+        return self.title
--- Photos/tests.py
+++ Photos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from .models import Category
+
 # Create your tests here.
+class CategoryTest(TestCase):
+
+    def test_create(self):
+        category = Category(name="Test Category")
+
+        # test __str__ method
+        self.assertEqual(str(category), "Test Category")
+
+        # name 
+        self.assertEqual(category.name, "Test Category")
