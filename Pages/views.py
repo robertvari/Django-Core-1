25da7ecfb7dcc12a08bfaca0436ee60602daffe5
Robert Vari <mail.robertvari@gmail.com>
2019-11-02 13:09:40 +0100

    Image grid CSS

--- Pages/views.py
+++ Pages/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render, HttpResponse
 from Photos.models import Photo
 
 # Create your views here.
+def get_random_photos():
+    '''
+    Get random photos for testing CSS grid
+    '''
+    
+    photo_list = []
+    for index in range(20):
+        photo_list.append(
+            {
+                "name": f"Photo {index}", 
+                "url":f"https://source.unsplash.com/1100x600/?nature{index}",
+                "description": f"Photo {index} description..."
+            }
+        )
+    
+    return photo_list
+
 def home(request):
     context = {
         "photos": Photo.objects.all()
