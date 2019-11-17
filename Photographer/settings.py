23575f03a247e07b65defb828a8641c622c26cb1
Robert Vari <mail.robertvari@gmail.com>
2019-11-17 12:52:56 +0100

    Environment variables for API keys

--- Photographer/settings.py
+++ Photographer/settings.py
@@ -19,7 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gqm&76t0$+ap7rwr(tdzvht@36)d*5tcizp*w+ouyf%tbiy&wi'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
