16b2f1d30aa78f4fdcd069291ab1f44205db9b8e
Robert Vari <mail.robertvari@gmail.com>
2019-11-17 09:21:38 +0100

    New model: About

--- Pages/models.py
+++ Pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    name = models.CharField("Név", max_length=100)
+    profile_pic = models.ImageField("Profilkép", upload_to="about")
+
+    faceebook = models.URLField("Facebook URL", max_length=200, blank=True)
+    instagram = models.URLField("Instagram URL", max_length=200, blank=True)
+    phone = models.CharField("Telefon", max_length=200, blank=True)
+    
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Magamról"
+    
+    def __str__(self):
+        return self.name
